d752232c1f4e53fd35059e8796eb88a06e97a0d9
Everton <evertonoliveirabispo30@gmail.com>
2023-05-19 17:23:43 -0300

    Treino Gerenciado de Usuarios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Listagem from "./Componentes/Listagem"
 
 function App() {
+
+    // Como funciona objetos em JS
+    const usuario = {
+        // chave: valor
+        nome: "Everton",
+        idade: 18,
+        status: true,
+        endereco: {
+            rua: "Rua X",
+            numero: 139,
+            complemento: "ap 20"
+        },
+        vacinasCovid19: ["Pfizer", "Pfizer", "Pfizer", "J&J"]
+    }
+
+    // Contabiliza quantos itens tem dentro do array
+    const qtdVacina = usuario.vacinasCovid19.length - 1
+
+    //console.log(usuario)
+    //console.log(usuario.endereco.rua, usuario.endereco.numero)
+    // console.log(Object.keys(usuario))
+    
+    // Como usar um array com objetos dentro
+    const usuarios = [
+        {nome: "Lores", status: false},
+        {nome: "Arthur", status: true},
+        {nome: "Zezin", status: true},
+        {nome: "Kelvin", status: true}
+    ]
+
+    console.log(usuarios)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    
+        <Listagem usuarios={usuarios}/>
+
+        <div className="d-flex justify-content-around">
+
+            <div className="caixa">
+                <h2>Cadastro</h2>
+            </div>
+
+        </div>
+        
+
     </div>
   );
 }
+
+
 
 export default App;
