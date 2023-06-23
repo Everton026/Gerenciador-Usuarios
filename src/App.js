85f889f530806bcc41a6db4ffb4fa1528e980d6b
Everton <evertonoliveirabispo30@gmail.com>
2023-06-23 17:16:42 -0300

    bd

--- src/App.js
+++ src/App.js
@@ -4,61 +4,47 @@ import Cadastro from "./Componentes/Cadastro";
 import Listagem from "./Componentes/Listagem";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import supabase from "./supabase";
 
 function App() {
 
-    // Como funciona objetos em JS
-    const usuario = {
-        // chave: valor
-        nome: "Everton",
-        idade: 18,
-        status: true,
-        endereco: {
-            rua: "Rua X",
-            numero: 139,
-            complemento: "ap 20"
-        },
-        vacinasCovid19: ["Pfizer", "Pfizer", "Pfizer", "J&J"]
-    }
-
-    // Contabiliza quantos itens tem dentro do array
-    const qtdVacina = usuario.vacinasCovid19.length - 1
-
-    // console.log(usuario)
-    // console.log(usuario.endereco.rua, usuario.endereco.numero)
-    // console.log(Object.keys(usuario))
-    
-    // Como usar um array com objetos dentro
-    const usuarios_padrao = [
-        {nome: "Lores", email:"lolis@gmail.com", status: false},
-        {nome: "Arthur", email:"arthurdoval@gmail.com", status: true},
-        {nome: "Zezin",  email:"ainzedamanga@gmail.com", status: true},
-        {nome: "Kelvin",  email:"kevinx@gmail.com", status: true}
-    ]
-
-     const [usuarios, setUsuarios] = React.useState(usuarios_padrao);
+     const [usuarios, setUsuarios] = React.useState([buscaTodos]);
 
      const [telaEdita, setTelaEdita] = React.useState(false);
 
-
-    //console.log(usuarios)
+     function buscaTodos(){
+        supabase.from("Usuarios").select()
+        .then(reponse => {
+            console.log("Conexão bem sucedida!")
+            console.log(reponse)
+            setUsuarios(reponse.data)
+        })
+        .catch(reponse => {
+            console.log("Deu erro na conexão!")
+            console.log(reponse)
+        });
+     }
+     
+     React.useEffect(()=>{
+        buscaTodos();
+    },[])
 
   return (
 
     <div className="container">
     
-                    <ToastContainer
-                        position="top-right"
-                        autoClose={5000}
-                        hideProgressBar={false}
-                        newestOnTop={false}
-                        closeOnClick
-                        rtl={false}
-                        pauseOnFocusLoss
-                        draggable
-                        pauseOnHover
-                        theme="dark"
-                    />
+        <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+        />
 
         <div className="pai" >
 
